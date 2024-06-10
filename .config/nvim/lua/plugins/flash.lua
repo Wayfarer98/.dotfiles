4253a71c4ccce222a1c78633001581ae3bd2f042
Magnus Keinicke <magnus@skraentegaarden.dk>
2024-06-10 08:53:44 +0200

    Update flash to not use "S" key, and disable backdrop

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,14 +2,20 @@ local M = {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		label = {
+			after = false,
+			before = true,
+		},
+		highlight = {
+			backdrop = false
+		}
+	},
 	-- stylua: ignore
 	keys = {
-		{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-		{ "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-		{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-		{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-		{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+		{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+		{ "r",     mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+		{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end, desc = "Toggle Flash Search" },
 	},
 }
 
