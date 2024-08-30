6da0a5efb485369bce736c3ed259e17020d52a88
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-08-30 22:25:23 +0200

    Set diagnostics to undercurl

--- .config/nvim/lua/after/options.lua
+++ .config/nvim/lua/after/options.lua
@@ -1,2 +1,10 @@
 -- Flash label color
 vim.api.nvim_set_hl(0, 'FlashLabel', { fg = 'red' })
+
+-- Diagnostic colors and underlines
+vim.cmd [[
+  highlight DiagnosticUnderlineError gui=undercurl
+  highlight DiagnosticUnderlineWarn gui=undercurl
+  highlight DiagnosticUnderlineInfo gui=undercurl
+  highlight DiagnosticUnderlineHint gui=undercurl
+]]
