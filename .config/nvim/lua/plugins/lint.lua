231fa898281e511e001c1d5e101e667593c12f5f
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-08-30 15:08:49 +0200

    Beautification

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -5,18 +5,11 @@ local M = {
     config = function()
       local lint = require 'lint'
 
-      lint.linters_by_ft = lint.linters_by_ft or {}
-      lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      lint.linters_by_ft['clojure'] = nil
-      lint.linters_by_ft['dockerfile'] = nil
-      lint.linters_by_ft['inko'] = nil
-      lint.linters_by_ft['janet'] = nil
-      lint.linters_by_ft['json'] = nil
-      lint.linters_by_ft['markdown'] = nil
-      lint.linters_by_ft['rst'] = nil
-      lint.linters_by_ft['ruby'] = nil
-      lint.linters_by_ft['terraform'] = nil
-      lint.linters_by_ft['text'] = nil
+      lint.linters_by_ft = {
+        haskell = { 'hlint' },
+        json = nil,
+        text = nil,
+      }
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
