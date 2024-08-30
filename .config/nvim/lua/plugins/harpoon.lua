ef5c9100a7ad1fbb11402de9986d8406b8ba57f7
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-08-30 15:08:29 +0200

    Fix harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -2,42 +2,55 @@ local M = {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local harpoon = require 'harpoon'
-
-    harpoon:setup()
-    --
-    -- Keybindings
-    vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():add()
-    end, { desc = '[H]arpoon [A]dd the current buffer into the harpoon list' })
-    vim.keymap.set('n', '<leader>ht', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = '[H]arpoon [T]oggle menu' })
-
-    vim.keymap.set('n', '<c-1>', function()
-      harpoon:list():select(1)
-    end, { desc = 'Select buffer 1 in harpoon list' })
-    vim.keymap.set('n', '<c-2>', function()
-      harpoon:list():select(2)
-    end, { desc = 'Select buffer 2 in harpoon list' })
-    vim.keymap.set('n', '<c-3>', function()
-      harpoon:list():select(3)
-    end, { desc = 'Select buffer 3 in harpoon list' })
-    vim.keymap.set('n', '<c-4>', function()
-      harpoon:list():select(4)
-    end, { desc = 'Select buffer 4 in harpoon list' })
-    vim.keymap.set('n', '<c-5>', function()
-      harpoon:list():select(5)
-    end, { desc = 'Select buffer 5 in harpoon list' })
-
-    vim.keymap.set('n', '<tab>', function()
-      harpoon:list():next()
-    end, { desc = 'Select next buffer in harpoon list' })
-
-    vim.keymap.set('n', '<S-tab>', function()
-      harpoon:list():next()
-    end, { desc = 'Select previous buffer in harpoon list' })
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = function()
+    local keys = {
+      {
+        '<leader>ha',
+        function()
+          require('harpoon'):list():add()
+        end,
+        desc = 'Add the current buffer into the harpoon list',
+      },
+      {
+        '<leader>ht',
+        function()
+          require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
+        end,
+        desc = 'Toggle harpoon menu',
+      },
+      {
+        '<Tab>',
+        function()
+          require('harpoon'):list():next { ui_nav_wrap = true }
+        end,
+        desc = 'Go to next buffer in harpoon list',
+      },
+      {
+        '<S-Tab>',
+        function()
+          require('harpoon'):list():prev { ui_nav_wrap = true }
+        end,
+        desc = 'Go to previous buffer in harpoon list',
+      },
+    }
+    for i = 1, 5 do
+      table.insert(keys, {
+        '<leader>' .. i,
+        function()
+          require('harpoon'):list():select(i)
+        end,
+        desc = 'Select buffer ' .. i .. ' in harpoon list',
+      })
+    end
+    return keys
   end,
 }
 return M
