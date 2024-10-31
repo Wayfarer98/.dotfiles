f0c82a0d23790601c40897027e84c5d9d0d9e08c
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-10-31 07:49:54 +0100

    Updates and add dotnetui and leetcode plugins

---
+++ .config/nvim/lua/plugins/dotnet.lua
@@ -0,0 +1,7 @@
+local M = {
+  'MoaidHathot/dotnet.nvim',
+  cmd = 'DotnetUI',
+  opts = {},
+}
+
+return M
---
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,20 @@
+local M = {
+  'kawre/leetcode.nvim',
+  build = ':TSUpdate html',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'nvim-lua/plenary.nvim', -- required by telescope
+    'MunifTanjim/nui.nvim',
+
+    -- optional
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  opts = {
+    plugins = {
+      non_standalone = true,
+    },
+  },
+}
+
+return M
