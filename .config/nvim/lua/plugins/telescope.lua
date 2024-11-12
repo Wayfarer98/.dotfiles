da86704acb8efd6440266dab96c8fe209fa99019
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-11-12 16:46:00 +0100

    <leader>st now browses wezterm config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -67,7 +67,11 @@ local M = { -- Fuzzy Finder (files, lsp, etc)
         current_dir = vim.fn.fnamemodify(current_file, ':h')
       end
 
-      local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+      local git_root = vim.fn.systemlist(
+        'git -C '
+          .. vim.fn.escape(current_dir, ' ')
+          .. ' rev-parse --show-toplevel'
+      )[1]
       if vim.v.shell_error ~= 0 then
         print 'Not a git repository. Searching cwd'
         return cwd
@@ -99,30 +103,82 @@ local M = { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sF', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set(
+      'n',
+      '<leader>sh',
+      builtin.help_tags,
+      { desc = '[S]earch [H]elp' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>sk',
+      builtin.keymaps,
+      { desc = '[S]earch [K]eymaps' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>sF',
+      builtin.find_files,
+      { desc = '[S]earch [F]iles' }
+    )
     vim.keymap.set('n', '<leader>sf', function()
       search_files_git_root(builtin)
     end, { desc = '[S]earch [F]iles in git root' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sG', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set(
+      'n',
+      '<leader>ss',
+      builtin.builtin,
+      { desc = '[S]earch [S]elect Telescope' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>sw',
+      builtin.grep_string,
+      { desc = '[S]earch current [W]ord' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>sG',
+      builtin.live_grep,
+      { desc = '[S]earch by [G]rep' }
+    )
     vim.keymap.set('n', '<leader>sg', function()
       live_grep_git_root(builtin)
     end, { desc = '[S]earch by [G]rep in git root' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set(
+      'n',
+      '<leader>sd',
+      builtin.diagnostics,
+      { desc = '[S]earch [D]iagnostics' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>sr',
+      builtin.resume,
+      { desc = '[S]earch [R]esume' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>s.',
+      builtin.oldfiles,
+      { desc = '[S]earch Recent Files ("." for repeat)' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader><leader>',
+      builtin.buffers,
+      { desc = '[ ] Find existing buffers' }
+    )
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        -- winblend = 10,
-        previewer = false,
-      })
+      builtin.current_buffer_fuzzy_find(
+        require('telescope.themes').get_dropdown {
+          -- winblend = 10,
+          previewer = false,
+        }
+      )
     end, { desc = '[/] Fuzzily search in current buffer' })
 
     -- It's also possible to pass additional configuration options.
@@ -138,6 +194,10 @@ local M = { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
+
+    vim.keymap.set('n', '<leader>st', function()
+      builtin.find_files { cwd = vim.fn.expand '$HOME/.config/wezterm' }
+    end, { desc = '[S]earch [T]erminal configuration files' })
   end,
 }
 return M
