59a9360d6e625cd3172e9a49ebd20a6a1bbd8a29
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-11-13 13:19:04 +0100

    Add PPD workspace

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -29,13 +29,43 @@ local function create_workspace(name, tabs)
 end
 
 wezterm.on('gui-startup', function()
+  -- Main workspace
   local mainTabs = {
     { title = 'Terminal', directory = os.getenv 'HOME' },
     { title = 'Code', directory = os.getenv 'HOME', command = 'nvim' },
   }
   local window = create_workspace('Main', mainTabs)
-  create_workspace('AP', mainTabs)
+
+  -- PPD workspace
+  local PPD_tabs = {
+    {
+      title = 'Terminal',
+      directory = os.getenv 'HOME' .. '/Documents/Repos/Kandidat/PPD',
+      command = 'source venv/bin/activate',
+    },
+    {
+      title = 'Code',
+      directory = os.getenv 'HOME' .. '/Documents/Repos/Kandidat/PPD',
+      command = 'source venv/bin/activate\nnvim',
+    },
+  }
+  create_workspace('PPD', PPD_tabs)
+
+  -- TA workspace
+  local TA_tabs = {
+    {
+      title = 'Terminal',
+      directory = os.getenv 'HOME' .. '/Documents/Repos/TA',
+    },
+    {
+      title = 'Code',
+      directory = os.getenv 'HOME' .. '/Documents/Repos/TA',
+      command = 'nvim',
+    },
+  }
   create_workspace('TA', mainTabs)
+
+  -- Start out in the 'Main' workspace and maximize the window
   mux.set_active_workspace 'Main'
   window:gui_window():maximize()
 end)
@@ -51,13 +81,61 @@ config.window_decorations = 'RESIZE'
 config.font = wezterm.font('JetBrains Mono', { weight = 'Regular' })
 config.font_size = 14
 
+-- setup neovim navigation and pane resizing integrations
+local function is_vim(pane)
+  local process_info = pane:get_foreground_process_info()
+  local process_name = process_info and process_info.name
+
+  return process_name == 'nvim' or process_name == 'vim'
+end
+
+local direction_keys = {
+  Left = 'h',
+  Down = 'j',
+  Up = 'k',
+  Right = 'l',
+  -- reverse lookup
+  h = 'Left',
+  j = 'Down',
+  k = 'Up',
+  l = 'Right',
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = {
+            key = key,
+            mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+          },
+        }, pane)
+      else
+        if resize_or_move == 'resize' then
+          win:perform_action(
+            { AdjustPaneSize = { direction_keys[key], 3 } },
+            pane
+          )
+        else
+          win:perform_action(
+            { ActivatePaneDirection = direction_keys[key] },
+            pane
+          )
+        end
+      end
+    end),
+  }
+end
+
+local nav_keys = {}
+
 -- set keybinds
 config.keys = {
   -- Pane navigation and splitting
-  { key = 'l', mods = 'CTRL', action = act.ActivatePaneDirection 'Right' },
-  { key = 'h', mods = 'CTRL', action = act.ActivatePaneDirection 'Left' },
-  { key = 'j', mods = 'CTRL', action = act.ActivatePaneDirection 'Down' },
-  { key = 'k', mods = 'CTRL', action = act.ActivatePaneDirection 'Up' },
   {
     key = '\\',
     mods = 'LEADER',
@@ -73,6 +151,16 @@ config.keys = {
     mods = 'LEADER',
     action = act.CloseCurrentPane { confirm = false },
   },
+  -- move between split panes
+  split_nav('move', 'h'),
+  split_nav('move', 'j'),
+  split_nav('move', 'k'),
+  split_nav('move', 'l'),
+  -- resize panes
+  split_nav('resize', 'h'),
+  split_nav('resize', 'j'),
+  split_nav('resize', 'k'),
+  split_nav('resize', 'l'),
   -- Adjust pane size
   { key = 'l', mods = 'LEADER', action = act.AdjustPaneSize { 'Right', 5 } },
   { key = 'h', mods = 'LEADER', action = act.AdjustPaneSize { 'Left', 5 } },
@@ -140,10 +228,6 @@ for i = 1, 9 do
   table.insert(config.keys, {
     key = tostring(i),
     mods = 'LEADER',
-    action = act.ActivateTab(i - 1),
-  })
-  table.insert(config.keys, {
-    key = 'F' .. tostring(i),
     action = act.ActivateTab(i - 1),
   })
 end
