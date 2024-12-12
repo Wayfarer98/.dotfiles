ad4345c131109d23a4570e54aa92f4b2693ea3e4
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-12-12 16:15:45 +0100

    New ISPC autocommand

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -3,7 +3,10 @@
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup(
+    'kickstart-highlight-yank',
+    { clear = true }
+  ),
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -21,7 +24,16 @@ vim.api.nvim_create_autocmd('BufRead', {
 })
 
 -- Set correct filetype for fsharp files
-vim.api.nvim_create_autocmd({'BufNewFile','BufRead'}, {
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   pattern = '*.fs,*.fsx,*.fsi',
   command = [[set filetype=fsharp]],
+})
+
+-- Set correct filetype for ispc files
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = '*.ispc',
+  callback = function()
+    vim.cmd [[set filetype=ispc]]
+    vim.cmd [[setlocal shiftwidth=2 tabstop=2 softtabstop=2 expandtab commentstring=//%s]]
+  end,
 })
