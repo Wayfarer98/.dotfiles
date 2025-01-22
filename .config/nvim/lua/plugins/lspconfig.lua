4255ae1c33b8bf63cd0aac68249767bc7e37e8d7
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-01-22 12:51:37 +0100

    comments

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -42,10 +42,7 @@ local M = { -- LSP Configuration & Plugins
     { 'Bilal2453/luvit-meta', lazy = true },
   },
   config = function()
-    --  This function gets run when an LSP attaches to a particular buffer.
-    --    That is to say, every time a new file is opened that is associated with
-    --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-    --    function will be executed to configure the current buffer
+    -- Configure keybinds for the LSP attached to the buffer
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup(
         'kickstart-lsp-attach',
