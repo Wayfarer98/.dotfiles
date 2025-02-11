85395149c0774e152182fb326ce11f5873f7334f
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-02-11 09:52:30 +0100

    Use defaults for gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,15 +1,7 @@
-local M = { -- Adds git related signs to the gutter, as well as utilities for managing changes
-  'lewis6991/gitsigns.nvim',
-  event = { 'BufRead', 'BufNewFile' },
-  opts = {
-    signs = {
-      add = { text = '│' },
-      change = { text = '│' },
-      delete = { text = '󰍵' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-      untracked = { text = '│' },
-    },
-  },
-}
+local M =
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufRead', 'BufNewFile' },
+    opts = {},
+  }
 return M
