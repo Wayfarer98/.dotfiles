c61e8b089337573c28b026d568b1a7ae7062ac56
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-02-11 09:47:00 +0100

    Formatting

--- .config/nvim/lua/plugins/todo-comment.lua
+++ .config/nvim/lua/plugins/todo-comment.lua
@@ -1,2 +1,7 @@
-local M = { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+local M = {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false },
+}
 return M
