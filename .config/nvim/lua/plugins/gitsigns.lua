9c6ebc79a2c8e72950421e6c0e59cc087dbae0d1
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-02-21 22:30:54 +0100

    Not needed anymore

--- .config/nvim/lua/plugins/gitsigns.lua
+++
@@ -1,7 +0,0 @@
-local M =
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    event = { 'BufRead', 'BufNewFile' },
-    opts = {},
-  }
-return M
