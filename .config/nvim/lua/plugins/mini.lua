8dd8809ee30e5092c4a78a5486da0b3a5e141d3f
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-02-21 22:31:18 +0100

    Add many mini tools

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,28 +1,46 @@
 local M = { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
+    local opts = require 'config.mini-opts'
+    -- Text editing
+    require('mini.icons').setup()
     require('mini.ai').setup { n_lines = 500 }
+    require('mini.comment').setup()
+    require('mini.move').setup()
+    require('mini.operators').setup()
+    require('mini.pairs').setup()
+    require('mini.splitjoin').setup()
+    require('mini.surround').setup()
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - yaiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - yd'   - [S]urround [D]elete [']quotes
-    -- - yr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup {
-      mappings = {
-        add = 'yn',
-        delete = 'yd',
-        replace = 'yr',
-        find = 'yf',
-        find_left = 'yF',
-        highlight = 'yh',
-        update_n_lines = 'yl',
+    -- General workflow
+    require('mini.files').setup(opts.files)
+    require('mini.jump').setup()
+    require('mini.jump2d').setup(opts.jump2d)
+    require('mini.git').setup()
+
+    -- Appearance
+    require('mini.indentscope').setup(opts.indentscope)
+    require('mini.hipatterns').setup(opts.hipatterns)
+    require('mini.starter').setup()
+    require('mini.diff').setup(opts.diff)
+  end,
+  keys = function()
+    local files = require 'mini.files'
+    return {
+      {
+        '\\',
+        function()
+          if files.get_explorer_state() then
+            files.close()
+          else
+            if vim.fn.expand '%' == 'Starter' then
+              files.open()
+            else
+              files.open(vim.api.nvim_buf_get_name(0))
+            end
+          end
+        end,
+        desc = 'open mini file explorer',
       },
     }
   end,
