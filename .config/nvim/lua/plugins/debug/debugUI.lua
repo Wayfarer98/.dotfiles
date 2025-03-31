9a5d8fcc668e14647947a0683a5e1d28f21880d5
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-03-31 14:57:05 +0200

    Don't remember why

--- .config/nvim/lua/plugins/debug/debugUI.lua
+++ .config/nvim/lua/plugins/debug/debugUI.lua
@@ -12,29 +12,47 @@ end
 M.setup_icons = function()
   local dapui = require 'dapui'
   local sign = vim.fn.sign_define
+  local C = require('catppuccin.palettes').get_palette 'mocha'
 
   ---@diagnostic disable-next-line: missing-fields
   dapui.setup {
     icons = { expanded = '', collapsed = '', current_frame = '' },
   }
 
-  sign(
-    'DapBreakpoint',
-    { text = '●', texthl = 'DapBreakpoint', linehl = '', numhl = '' }
-  )
+  sign('DapBreakpoint', {
+    text = '●',
+    texthl = 'DapBreakpoint',
+    linehl = '',
+    numhl = '',
+    priority = 11,
+  })
   sign('DapBreakpointCondition', {
     text = '●',
     texthl = 'DapBreakpointCondition',
     linehl = '',
     numhl = '',
+    priority = 11,
   })
-  sign(
-    'DapLogPoint',
-    { text = '◆', texthl = 'DapLogPoint', linehl = '', numhl = '' }
-  )
+  sign('DapLogPoint', {
+    text = '◆',
+    texthl = 'DapLogPoint',
+    linehl = '',
+    numhl = '',
+    priority = 11,
+  })
   -- Remove the sign and highlight the line instead
-  vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
-  sign('DapStopped', { text = '', linehl = 'DapStoppedLine', numhl = '' })
+  vim.api.nvim_set_hl(
+    0,
+    'DapStoppedLine',
+    { default = true, bg = 'NvimDarkYellow' }
+  )
+  vim.api.nvim_set_hl(0, 'DapStoppedText', { default = true, bg = C.peach })
+  sign('DapStopped', {
+    text = '',
+    texthl = 'DapStoppedText',
+    linehl = 'DapStoppedLine',
+    numhl = '',
+  })
 end
 
 return M
