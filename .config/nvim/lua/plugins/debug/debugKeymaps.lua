e89322c9bcf7f09bfc91768b3918532cbc08b0df
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-03-31 14:56:50 +0200

    Also use F keys for debugging

--- .config/nvim/lua/plugins/debug/debugKeymaps.lua
+++ .config/nvim/lua/plugins/debug/debugKeymaps.lua
@@ -127,6 +127,10 @@ M.keys = function()
       mode = { 'n', 'v' },
       desc = 'Eval',
     },
+    { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
+    { '<F1>', dap.step_over, desc = 'Debug: Step Over' },
+    { '<F3>', dap.step_into, desc = 'Debug: Step Into' },
+    { '<F4>', dap.step_out, desc = 'Debug: Step Out' },
   }
 end
 
